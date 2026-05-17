0c18d8670856c243a4c275484714c26d0a0fa8cf
Kyle <kountdown@mail.ru>
2026-05-17 14:56:21 +0200

    Clocks and simple clockwise priority pass

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,320 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const EdhChessClockApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class EdhChessClockApp extends StatelessWidget {
+  const EdhChessClockApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'EDH Chess Clock',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.green),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xff1f8a70)),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const ClockTableScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class ClockTableScreen extends StatefulWidget {
+  const ClockTableScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ClockTableScreen> createState() => _ClockTableScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ClockTableScreenState extends State<ClockTableScreen> {
+  static const _startingTime = Duration(minutes: 15);
+  static const _clockwiseLayout = <int>[0, 1, 3, 2];
+  static const _playerColors = <Color>[
+    Color(0xffff0051),
+    Color(0xff4652ff),
+    Color(0xffeb83ec),
+    Color(0xffffbd08),
+  ];
 
-  void _incrementCounter() {
+  final List<Duration> _remaining = List.generate(4, (_) => _startingTime);
+  late final Timer _timer;
+  int _activePlayer = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (_) => _tickClock());
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    _resetClocks();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void _passPriority(int playerIndex) {
+    if (playerIndex != _activePlayer) {
+      return;
+    }
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _activePlayer = (_activePlayer + 1) % _remaining.length;
+    });
+  }
+
+  void _tickClock() {
+    final currentTime = _remaining[_activePlayer];
+    if (currentTime == Duration.zero) {
+      return;
+    }
+
+    setState(() {
+      final nextTime = currentTime - const Duration(seconds: 1);
+      _remaining[_activePlayer] = nextTime.isNegative ? Duration.zero : nextTime;
+    });
+  }
+
+  void _resetClocks() {
+    setState(() {
+      for (var index = 0; index < _remaining.length; index++) {
+        _remaining[index] = _startingTime;
+      }
+      _activePlayer = 0;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+      backgroundColor: const Color(0xff111318),
+      body: SafeArea(
+        child: Stack(
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(child: _buildClockTile(0)),
+                        const SizedBox(width: 10),
+                        Expanded(child: _buildClockTile(1)),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        Expanded(child: _buildClockTile(2)),
+                        const SizedBox(width: 10),
+                        Expanded(child: _buildClockTile(3)),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: const Color(0xff111318),
+                  borderRadius: BorderRadius.circular(24),
+                  border: Border.all(color: Colors.black, width: 8),
+                ),
+                child: IconButton(
+                  tooltip: 'Reset clocks',
+                  color: Colors.white,
+                  iconSize: 40,
+                  icon: const Icon(Icons.casino),
+                  onPressed: _resetClocks,
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget _buildClockTile(int gridIndex) {
+    final playerIndex = _clockwiseLayout[gridIndex];
+    return PlayerClockTile(
+      color: _playerColors[playerIndex],
+      remaining: _remaining[playerIndex],
+      isActive: playerIndex == _activePlayer,
+      playerNumber: playerIndex + 1,
+      quarterTurns: gridIndex.isEven ? 1 : 3,
+      onTap: () => _passPriority(playerIndex),
+    );
+  }
+}
+
+class PlayerClockTile extends StatelessWidget {
+  const PlayerClockTile({
+    super.key,
+    required this.color,
+    required this.remaining,
+    required this.isActive,
+    required this.playerNumber,
+    required this.quarterTurns,
+    required this.onTap,
+  });
+
+  final Color color;
+  final Duration remaining;
+  final bool isActive;
+  final int playerNumber;
+  final int quarterTurns;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final markColor = Colors.black.withValues(alpha: 0.28);
+    final tile = AnimatedContainer(
+      duration: const Duration(milliseconds: 180),
+      curve: Curves.easeOut,
+      decoration: BoxDecoration(
+        color: isActive ? color : Color.alphaBlend(Colors.white10, color),
+        borderRadius: BorderRadius.circular(30),
+        border: Border.all(
+          color: isActive ? Colors.black : Colors.transparent,
+          width: isActive ? 6 : 0,
+        ),
+        boxShadow: [
+          if (isActive)
+            BoxShadow(
+              color: Colors.white.withValues(alpha: 0.3),
+              blurRadius: 18,
+              spreadRadius: 1,
+            ),
+        ],
       ),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(30),
+          onTap: onTap,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Positioned(
+                left: 46,
+                top: 0,
+                bottom: 0,
+                child: Center(
+                  child: _DecorativeMark(icon: Icons.add, color: markColor),
+                ),
+              ),
+              Positioned(
+                right: 46,
+                top: 0,
+                bottom: 0,
+                child: Center(
+                  child: _DecorativeMark(icon: Icons.remove, color: markColor),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 44),
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Text(
+                      _formatDuration(remaining),
+                      key: ValueKey('player-$playerNumber-timer'),
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontFeatures: [FontFeature.tabularFigures()],
+                        fontSize: 128,
+                        fontWeight: FontWeight.w900,
+                        height: 0.9,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: 24,
+                top: 18,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 180),
+                  opacity: isActive ? 1 : 0.46,
+                  child: Text(
+                    'P$playerNumber',
+                    style: TextStyle(
+                      color: Colors.black.withValues(alpha: 0.56),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w900,
+                      letterSpacing: 0,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                right: 22,
+                bottom: 18,
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 180),
+                  opacity: isActive ? 1 : 0,
+                  child: const Icon(
+                    Icons.play_arrow_rounded,
+                    color: Colors.black,
+                    size: 34,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    if (quarterTurns == 0) {
+      return tile;
+    }
+
+    return RotatedBox(quarterTurns: quarterTurns, child: tile);
+  }
+
+  static String _formatDuration(Duration duration) {
+    final totalSeconds = duration.inSeconds;
+    final hours = totalSeconds ~/ 3600;
+    final minutes = (totalSeconds % 3600) ~/ 60;
+    final seconds = totalSeconds % 60;
+
+    if (hours > 0) {
+      return '$hours:${_twoDigits(minutes)}:${_twoDigits(seconds)}';
+    }
+
+    return '$minutes:${_twoDigits(seconds)}';
+  }
+
+  static String _twoDigits(int value) => value.toString().padLeft(2, '0');
+}
+
+class _DecorativeMark extends StatelessWidget {
+  const _DecorativeMark({required this.icon, required this.color});
+
+  final IconData icon;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return IgnorePointer(
+      child: Icon(icon, color: color, size: 52),
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,32 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:edh_chess_clock/main.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:edh_chess_clock/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('active player timer advances and passes clockwise', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(const EdhChessClockApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('P1'), findsOneWidget);
+    expect(find.text('P2'), findsOneWidget);
+    expect(find.text('15:00'), findsNWidgets(4));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump(const Duration(seconds: 1));
+
+    expect(find.text('14:59'), findsOneWidget);
+    expect(find.text('15:00'), findsNWidgets(3));
+
+    await tester.tap(find.byKey(const ValueKey('player-1-timer')));
+    await tester.pump();
+    await tester.pump(const Duration(seconds: 1));
+
+    expect(find.text('14:59'), findsNWidgets(2));
+    expect(find.text('15:00'), findsNWidgets(2));
+
+    await tester.tap(find.byTooltip('Reset clocks'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('15:00'), findsNWidgets(4));
   });
 }
